8db49d7603b4b114fac99491a9f8b5a5d93580b5
R_Singh <95725497+RitwikSingh2009@users.noreply.github.com>
2026-06-29 13:41:06 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
